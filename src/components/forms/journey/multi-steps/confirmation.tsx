63953ef85e5dd579da162946d933553e7c0c83c3
Tim Rayner <trayner3nrl@hotmail.com>
2022-07-26 23:19:37 +0100

    made some mobile responsive changes after mobile testing

--- src/components/forms/journey/multi-steps/confirmation.tsx
+++ src/components/forms/journey/multi-steps/confirmation.tsx
@@ -4,6 +4,7 @@ function Confirmation(props: any){
         props.prevStep();
     }
 
+    //fake XHR request to simulate posting the journey to others on liftshare 
     const postJourney = () => {
         var xhr = new XMLHttpRequest();
         xhr.open('POST', 'http://localhost:8080/route');
@@ -15,9 +16,9 @@ function Confirmation(props: any){
         <form onSubmit={() => postJourney()}>
             <h3> Nearly There! </h3>
             <p> 
-                Your Journey Takes You From {props.route.originText} to {props.route?.destinationText} {props.route?.departure ? 'on ' + props.route?.departure : 'however, a date has not been specified'} <br/>
+                Your Journey Takes You From {props.route.originText} to {props.route?.destinationText} {props.route?.departure ? 'on ' + props.route?.departure : 'however a date has not yet been specified.'} <br/>
                 <br/>
-                Share Your Journey To Find Your Liftshare Partner? 
+                Share your journey and find your Liftshare partner today. 
             </p>
 
             <div className="button-group">
